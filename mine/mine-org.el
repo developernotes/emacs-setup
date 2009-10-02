4fdc9275fe6e6d502bbb248b343017c5de58df01
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-10-02 13:31:02 -0500

    initial configure of remember with org-mode.

--- mine/mine-org.el
+++ mine/mine-org.el
@@ -5,8 +5,23 @@
 (require 'org-install)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cr" 'org-remember)
+
+
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+(setq org-directory "~/.emacs.d/org/")
+(setq org-agenda-files '("~/.emacs.d/org/gtd-items.org"))
 (setq org-log-done t)
 
 (defun gtd()
   (interactive)
   (find-file "~/.emacs.d/org/gtd-items.org"))
+
+
+(setq org-remember-templates
+      '(("Todo" ?t "* TODO %?\n %i\n %a" "~/.emacs.d/org/gtd-items.org" "Todo")
+        ("Inbox" ?i "* %?" "~/.emacs.d/org/gtd-items.org" "Inbox")
+        ("Misc Task" ?m "* TODO %? %^g\n" "~/.emacs.d/org/gtd-items.org" "Misc Tasks")))
