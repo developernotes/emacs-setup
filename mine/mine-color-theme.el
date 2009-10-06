56332da7271b5cb43ffb1d2c056a73b920b10be7
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-10-06 16:41:35 -0500

    Adding twilight color theme.

--- mine/mine-color-theme.el
+++ mine/mine-color-theme.el
@@ -1,6 +1,7 @@
 (provide 'mine-color-theme)
   (require 'color-theme)
-    (color-theme-initialize)
+  (color-theme-initialize)
 
-(require 'color-theme-vibrant-ink)
-(color-theme-vibrant-ink)
+(require 'color-theme-twilight)
+(color-theme-twilight)
+
