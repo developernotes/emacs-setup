6062b1d908b84e7cf9d9ebed23923c1b40257b3c
Nick Parker <nickp@developernotes.com>
2009-10-10 11:36:08 -0500

    Updating the coloring within org-mode, adding a few additional key bindings.

--- mine/mine-org.el
+++ mine/mine-org.el
@@ -7,21 +7,71 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cr" 'org-remember)
 
+(global-set-key (kbd "C-c g g") 'gtd)
+(global-set-key (kbd "C-c g a") 'gtd-switch-to-agenda)
 
 (setq remember-annotation-functions '(org-remember-annotation))
 (setq remember-handler-functions '(org-remember-handler))
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
 
-(setq org-directory "~/.emacs.d/org/")
-(setq org-agenda-files '("~/.emacs.d/org/gtd-items.org"))
+(setq org-directory "~/org/")
+(setq org-agenda-files '("~/org/gtd-items.org"))
 (setq org-log-done t)
 
 (defun gtd()
   (interactive)
-  (find-file "~/.emacs.d/org/gtd-items.org"))
+  (find-file "~/org/gtd-items.org"))
 
+(setq org-enforce-todo-dependencies t
+      org-todo-keywords
+      '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)"))
+      org-use-fast-todo-selection t)
 
 (setq org-remember-templates
-      '(("Todo" ?t "* TODO %?\n %i\n %a" "~/.emacs.d/org/gtd-items.org" "Todo")
-        ("Inbox" ?i "* %?" "~/.emacs.d/org/gtd-items.org" "Inbox")
-        ("Misc Task" ?m "* TODO %? %^g\n" "~/.emacs.d/org/gtd-items.org" "Misc Tasks")))
+      '(("Todo" ?t "* TODO %?\n %i\n %a" "gtd-items.org" "Todo")
+        ("Inbox" ?i "* %?" "gtd-items.org" "Inbox")
+        ("Misc Task" ?m "* TODO %? %^g\n" "gtd-items.org" "Misc Tasks")))
+
+(setq org-agenda-custom-commands
+      '(("A" "Action List"
+         ((agenda "")
+          (alltodo))
+         ((org-agenda-todo-ignore-deadlines t)
+          (org-agenda-todo-ignore-scheduled t)
+          (org-agenda-todo-ignore-with-date t)
+          (org-agenda-sorting-strategy '(tag-up))))))
+
+(defun gtd-switch-to-agenda ()
+  (interactive)
+  (if (get-buffer "*Org Agenda*")
+      (progn
+        (split-window-vertically)
+        (other-window 1)
+        (switch-to-buffer "*Org Agenda*")
+        (org-fit-agenda-window))
+      (org-agenda nil "A")))
+
+(custom-set-faces
+ '(outline-1 ((t (:foreground "#D6B163" :bold t))))
+ '(outline-2 ((t (:foreground "#A5F26E" :bold t))))
+ '(outline-3 ((t (:foreground "#B150E7" :bold nil))))
+ '(outline-4 ((t (:foreground "#529DB0" :bold nil))))
+ '(outline-5 ((t (:foreground "#CC7832" :bold nil))))
+ '(org-level-1 ((t (:inherit outline-1))))
+ '(org-level-2 ((t (:inherit outline-2))))
+ '(org-level-3 ((t (:inherit outline-3))))
+ '(org-level-4 ((t (:inherit outline-4))))
+ '(org-level-5 ((t (:inherit outline-5))))
+ '(org-agenda-date ((t (:inherit font-lock-type-face))))
+ '(org-agenda-date-weekend ((t (:inherit org-agenda-date))))
+ '(org-scheduled-today ((t (:foreground "#ff6ab9" :italic t))))
+ '(org-scheduled-previously ((t (:foreground "#d74b4b"))))
+ '(org-upcoming-deadline ((t (:foreground "#d6ff9c"))))
+ '(org-warning ((t (:foreground "#8f6aff" :italic t))))
+ '(org-date ((t (:inherit font-lock-constant-face))))
+ '(org-tag ((t (:inherit font-lock-comment-delimiter-face))))
+ '(org-hide ((t (:foreground "#191919"))))
+ '(org-todo ((t (:background "DarkRed" :foreground "white" :box (:line-width 1 :style released-button)))))
+ '(org-done ((t (:background "DarkGreen" :foreground "white" :box (:line-width 1 :style released-button)))))
+ '(org-column ((t (:background "#222222"))))
+ '(org-column-title ((t (:background "DarkGreen" :foreground "white" :bold t :box (:line-width 1 :style released-button))))))
