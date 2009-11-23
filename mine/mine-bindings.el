3059f0454d934591bd034199c35f78587ce8b866
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-11-23 02:11:00 -0600

    extending search to show for occurances.

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -2,3 +2,11 @@
 
 (global-set-key "\C-cv" 'mine-toggle-transparency)
 (global-set-key "\C-cw" 'toggle-truncate-lines)
+
+;; extend search to show occurances
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda ()
+    (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string
+               (regexp-quote isearch-string))))))
