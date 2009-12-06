0c9b82d89a9d2813d1392a388437d361b04f82f2
Nick Parker <nickp@developernotes.com>
2009-12-06 15:07:17 -0600

    setting default directory to ~

--- init.el
+++ init.el
@@ -40,3 +40,4 @@
 (message "My .emacs loaded in %ds." (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
 
 (gtd-agenda)
+(cd "~")
