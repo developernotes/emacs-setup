f322a61c1a8c5d772146ad44a70a2a90ba09e150
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-12-06 09:02:50 -0600

    adding additional keybinds to info-mode

---
+++ mine/mine-info-mode.el
@@ -0,0 +1,13 @@
+
+;; additional keyboard navigation
+(add-hook 'Info-mode-hook
+					'(lambda ()
+						 (define-key Info-mode-map [?j] 
+							 'next-line)))
+
+(add-hook 'Info-mode-hook
+					'(lambda ()
+						 (define-key Info-mode-map [?k]
+							 'previous-line)))
+
+(provide 'mine-info-mode)
