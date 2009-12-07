a65438955ddbf8e35cbc840c8cdc9d606a477540
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-12-07 16:03:53 -0600

    adding a before hook to adjust the filepath for a bookmark entry.

--- mine/mine-bookmark.el
+++ mine/mine-bookmark.el
@@ -1,4 +1,12 @@
 
 (require 'bookmark)
+
 (setq bookmark-default-file "~/bookmarks/emacs.bmk")
+
+(defadvice bookmark-save (before adjust-file-path activate compile)
+	"Adjusts the sicp bookmark to always use the tilde path operator over a fully qualified path"
+	(bookmark-prop-set 
+	 (bookmark-get-bookmark "sicp") 
+	 'filename "~/info/sicp.info"))
+
 (provide 'mine-bookmark)
