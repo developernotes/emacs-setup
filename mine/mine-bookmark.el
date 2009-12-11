2a72e40ed003f51649009b94f9721e97265b6c3a
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-12-11 14:40:39 -0600

    adding check to confirm the sicp bookmark exists before modification.

--- mine/mine-bookmark.el
+++ mine/mine-bookmark.el
@@ -5,8 +5,10 @@
 
 (defadvice bookmark-save (before adjust-file-path activate compile)
 	"Adjusts the sicp bookmark to always use the tilde path operator over a fully qualified path"
-	(bookmark-prop-set 
-	 (bookmark-get-bookmark "sicp") 
-	 'filename "~/info/sicp.info"))
+	(if (bookmark-get-bookmark "sicp")
+			(progn 
+				(bookmark-prop-set 
+				 (bookmark-get-bookmark "sicp") 
+				 'filename "~/info/sicp.info"))))
 
 (provide 'mine-bookmark)
