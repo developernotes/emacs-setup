0a049dcab01f1e49f1529e726142b2253371979a
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2009-12-29 10:12:18 -0600

    changing color theme

--- mine/mine-color-theme.el
+++ mine/mine-color-theme.el
@@ -2,7 +2,7 @@
 (require 'color-theme)
 (color-theme-initialize)
 
-(require 'color-theme-vibrant-ink)
-(color-theme-vibrant-ink)
+(require 'color-theme-twilight)
+(color-theme-twilight)
 
 (provide 'mine-color-theme)
