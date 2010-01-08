41f3ce89169b3cfe5445add14fe27a25d6695f26
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-01-08 05:18:59 -0600

    rebinding dired-find-alternate-file to enter so it doesn't create unnecessary buffers.

--- mine/mine-dired.el
+++ mine/mine-dired.el
@@ -1,4 +1,14 @@
 
+(put 'dired-find-alternate-file 'disabled nil)
+
+(add-hook 'dired-mode-hook
+ (lambda ()
+  (define-key dired-mode-map (kbd "<return>")
+    'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "^")
+    (lambda () (interactive) (find-alternate-file "..")))
+ ))
+
 (defun dired-open-files ()
   "Opens all marked files"
   (interactive)
