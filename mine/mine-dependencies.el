d5c5a10d86c3abf238d0f29969033ab92d2061ee
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-01-19 03:15:26 -0600

    removing yasnippet from ELPA, using a regular install now

--- mine/mine-dependencies.el
+++ mine/mine-dependencies.el
@@ -20,5 +20,18 @@
 	"Kill a rectangular region and save it in the kill ring." t)
 (autoload 'rm-kill-ring-save "rect-mark"
 	"Copy a rectangular region to the kill ring." t)
+
+;; yasnippet
+(add-path "site-lisp/yasnippet-0.6.1c")
+(require 'yasnippet)
+(yas/initialize)
+(setq yas/root-directories
+      (list (concat emacs-root "site-lisp/yasnippet-0.6.1c/snippets/")
+            (concat emacs-root "mysnippets/")))
+(mapc 'yas/load-directory yas/root-directories)
+
+(setq yas/prompt-functions '(yas/dropdown-prompt
+                             yas/ido-prompt
+                             yas/no-prompt))
 				
 (provide 'mine-dependencies)
