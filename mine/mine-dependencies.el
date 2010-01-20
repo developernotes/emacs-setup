d6f7cf5e7eb9414f6aa155afa3d1b46fa0d1c1c7
Nick Parker <nickp@developernotes.com>
2010-01-19 19:24:23 -0600

    adjusting yasnippet to work better in ruby-mode

--- mine/mine-dependencies.el
+++ mine/mine-dependencies.el
@@ -33,5 +33,18 @@
 (setq yas/prompt-functions '(yas/dropdown-prompt
                              yas/ido-prompt
                              yas/no-prompt))
+
+
+(defun yas/advise-indent-function (function-symbol)
+  (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
+           ,(format
+             "Try to expand a snippet before point, then call `%s' as usual"
+             function-symbol)
+           (let ((yas/fallback-behavior nil))
+             (unless (and (interactive-p)
+                          (yas/expand))
+               ad-do-it)))))
+
+(yas/advise-indent-function 'ruby-indent-line)
 				
 (provide 'mine-dependencies)
