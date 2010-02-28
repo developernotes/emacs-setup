21ed80e0b4dfc7585fde57a4f563f71ec54f6c58
Nick Parker <nickp@developernotes.com>
2010-02-28 09:31:42 -0600

    tail function

--- mine/mine-defuns.el
+++ mine/mine-defuns.el
@@ -6,7 +6,9 @@
 	  (buffer-list))
   (delete-other-windows))
 
-
-
+(defun tail ()
+	"tail the file loaded within the current buffer"
+	(interactive)
+	(auto-revert-tail-mode))
 
 (provide 'mine-defuns)
