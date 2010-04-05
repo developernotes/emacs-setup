2123f16a8b5de8d4a6b9c2b6a441dcf2b1276129
Nick Parker <nparker@geolearning.com>
2010-04-05 13:54:58 -0500

    adding another rakefile mapping for ruby-mode

--- mine/mine-ruby.el
+++ mine/mine-ruby.el
@@ -17,6 +17,7 @@
 ;;File type associations 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
 
