850d81f3bb4738f7f5865e0c7bd9e85893609843
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-04-11 10:41:37 -0500

    updating the exec-path to include the path to git.

--- mine/mine-osx.el
+++ mine/mine-osx.el
@@ -6,6 +6,8 @@
 	(append-to-environment-variable "PATH" "/usr/local/git/bin")
 	(append-to-environment-variable "PATH" "/opt/local/bin"))
 
+(add-to-list 'exec-path "/usr/local/git/bin")
+
 (setq doc-view-ghostscript-program "/usr/local/bin/gs")
 
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
