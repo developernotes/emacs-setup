0e243b365512a951edd6c55a3540642c13b405f3
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-04-27 21:51:17 -0500

    adding region/line movement.

--- mine/mine-navigation.el
+++ mine/mine-navigation.el
@@ -5,8 +5,49 @@
 (require 'bm)
 (require 'browse-kill-ring)
 
+;; Killring
 (browse-kill-ring-default-keybindings)
 
+;; Region/line movement
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg))
+        (forward-line -1))
+      (move-to-column column t)))))
+
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+
+(global-set-key [M-S-up] 'move-text-up)
+(global-set-key [M-S-down] 'move-text-down)
+
+;; Ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
@@ -27,6 +68,7 @@
 (global-set-key (kbd "C-<right>")  'bm-next)
 (global-set-key (kbd "C-<left>")   'bm-previous)
 
+;; Ido
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 (setq confirm-nonexistent-file-or-buffer -1)
