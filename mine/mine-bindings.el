8717e5856a5115757712dd317dbe2cf55699f724
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-06-02 14:02:01 -0500

    adding keybinding to load sql connection profiles

--- mine/mine-bindings.el
+++ mine/mine-bindings.el
@@ -1,11 +1,12 @@
 
 ;; misc
-(global-set-key "\C-cv" 'mine-toggle-transparency)
-(global-set-key "\C-cw" 'toggle-truncate-lines)
-(global-set-key "\C-cs" 'ido-goto-symbol)
-(global-set-key "\C-cp" 'mine-goto-symbol-at-point)
-(global-set-key "\M-g"  'goto-line)
-(global-set-key "\C-cn" 'clean-up-buffer-or-region)
+(global-set-key "\C-cv"     'mine-toggle-transparency)
+(global-set-key "\C-cw"     'toggle-truncate-lines)
+(global-set-key "\C-cs"     'ido-goto-symbol)
+(global-set-key "\C-cp"     'mine-goto-symbol-at-point)
+(global-set-key "\M-g"      'goto-line)
+(global-set-key "\C-cn"     'clean-up-buffer-or-region)
+(global-set-key "\C-c\C-p"  'sql-connection-profiles)
 
 ;; extend search to show occurances
 (define-key isearch-mode-map (kbd "C-o")
