7f7045f6c227e456e8db5deaa60372ef0f035f41
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-06-02 08:51:51 -0500

    updating the custom methods for sql support.

--- mine/mine-sql.el
+++ mine/mine-sql.el
@@ -1,12 +1,21 @@
 
+(defun sql-connection-profiles ()
+	"Display sql connection profiles"
+	(interactive)
+	(find-file (concat (getenv "HOME") "/org/sql.org")))
 
 (defun mine-mysql (user password host database)
   (let ((sql-user user)
         (sql-password password)
         (sql-server host)
-        (sql-database database))
-    (call-interactively 'sql-mysql)
-    (rename-buffer (concat "*SQL*:" database ":" host))))
-
+        (sql-database database)
+        (sql-buffer-name (concat "*SQL*:" database ":" host)))
+    (when (not (get-buffer sql-buffer-name))
+      (call-interactively 'sql-mysql)
+      (rename-buffer sql-buffer-name))
+    (delete-other-windows)
+    (switch-to-buffer sql-buffer-name)
+    (split-window-vertically)
+    (find-file (concat (getenv "HOME") "/Documents/sql-scripts/" database "_" host ".sql"))))
 
 (provide 'mine-sql)
