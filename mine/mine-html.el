71655fe3e5a712fc6d355153409906dc5d1e3056
Nick Parker <nparker@geolearning.com>
2010-06-08 10:26:38 -0500

    Loading html-mode for XHTML files.

--- mine/mine-html.el
+++ mine/mine-html.el
@@ -2,5 +2,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.textile$" . textile-mode))
+(add-to-list 'auto-mode-alist '("\\.xhtml$" . html-mode))
 
 (provide 'mine-html)
