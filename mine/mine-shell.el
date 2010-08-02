2284f14fd6da0da2d52fcb86048a3f8ac250a290
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-08-02 13:06:01 -0500

    reorganizing the file

--- mine/mine-shell.el
+++ mine/mine-shell.el
@@ -1,42 +1,38 @@
 (require 'multi-shell)
 
 (setq multi-shell-command "zsh")
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
 
 (global-set-key (kbd "C-c t") 'multi-shell-next)
 (global-set-key (kbd "C-c T") 'multi-shell-new)  ;; create a new one
 
-
-(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
-
 (add-hook 'shell-mode-hook 'n-shell-mode-hook)
+
 (defun n-shell-mode-hook ()
   "shell mode customizations."
   (local-set-key '[up] 'comint-previous-input)
   (local-set-key '[down] 'comint-next-input)
   (local-set-key '[(shift tab)] 'comint-next-matching-input-from-input)
-  (setq comint-input-sender 'n-shell-simple-send)
-  )
+  (setq comint-input-sender 'n-shell-simple-send))
 
 
 (defun n-shell-simple-send (proc command)
   "Various commands pre-processing before sending to shell."
   (cond
-   ;; Checking for clear command and execute it.
+   ;; checking for clear command and execute it.
    ((string-match "^[ \t]*clear[ \t]*$" command)
     (comint-send-string proc "\n")
-    (erase-buffer)
-    )
-   ;; Checking for man command and execute it.
+    (erase-buffer))
+
+   ;; checking for man command and execute it.
    ((string-match "^[ \t]*man[ \t]*" command)
     (comint-send-string proc "\n")
     (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
     (setq command (replace-regexp-in-string "[ \t]+$" "" command))
     ;;(message (format "command %s command" command))
-    (funcall 'man command)
-    )
-   ;; Send other commands to the default handler.
-   (t (comint-simple-send proc command))
-   )
-  )
+    (funcall 'man command))
+
+   ;; send other commands to the default handler.
+   (t (comint-simple-send proc command))))
 
 (provide 'mine-shell)
