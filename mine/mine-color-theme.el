ad6231a54a605091a3e859cd96e0861de71b97a6
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-08-31 19:40:50 -0500

    Changing the level 2 outline color for orgmode inside themes

--- mine/mine-color-theme.el
+++ mine/mine-color-theme.el
@@ -14,14 +14,16 @@
   (color-theme-standard)
   (set-face-background 'region "#ffec8b")
   (set-face-background 'hl-line "#b4eeb4")
-  (set-face-foreground 'font-lock-doc-face "#d2691e"))
+  (set-face-foreground 'font-lock-doc-face "#d2691e")
+  (custom-set-faces    '(outline-2 ((t (:foreground "#006400" :bold t))))))
 
 (defun mine-dark-color-theme ()
   (interactive)
   (color-theme-zen-and-art)
   (set-face-background 'hl-line "#333638")
   (set-face-background 'region "#999966")
-  (set-face-foreground 'font-lock-comment-face "#d8e5c5"))
+  (set-face-foreground 'font-lock-comment-face "#d8e5c5")
+  (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t))))))
 
 (mine-dark-color-theme)
 
