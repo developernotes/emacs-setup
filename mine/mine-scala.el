f58a60a2f2a13ed8a931309bbcfe140b79efa8ed
Nick Parker <nparker@nick-parkers-macbook-pro.local>
2010-09-13 21:02:42 -0500

    Adding ensime for scala

--- mine/mine-scala.el
+++ mine/mine-scala.el
@@ -2,6 +2,9 @@
 (add-path "site-lisp/scala")
 (require 'scala-mode-auto)
 
+(add-path "site-lisp/ensime/src/main/elisp")
+(require 'ensime)
 
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 (provide 'mine-scala)
