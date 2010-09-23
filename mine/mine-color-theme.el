c2718d893c016e1a8abf80dad22148ad9018da9f
Nick Parker <nparker@geolearning.com>
2010-09-23 18:48:12 -0500

    Moving custom theme settings into a hash-table, applying settings per theme name

--- mine/mine-color-theme.el
+++ mine/mine-color-theme.el
@@ -9,22 +9,35 @@
 
 (global-hl-line-mode t)
 
+(setq custom-theme-settings
+      (progn
+        (let ((theme-settings (make-hash-table :test 'equal)))
+          (puthash 'color-theme-standard
+                   '((set-face-background 'region "#ffec8b")
+                     (set-face-background 'hl-line "#b4eeb4")
+                     (set-face-foreground 'font-lock-doc-face "#d2691e")
+                     (custom-set-faces    '(outline-2 ((t (:foreground "#006400" :bold t))))))
+                   theme-settings)
+          (puthash 'color-theme-merbivore
+                   '((set-face-background 'hl-line "#3b3b3b")
+                     (set-face-background 'region "#999966")
+                     (set-face-foreground 'font-lock-comment-face "#d8e5c5")
+                     (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t))))))
+                   theme-settings)
+          theme-settings)))
+
+(defun apply-custom-theme-with-settings (theme)
+  (funcall theme)
+  (mapcar 'eval (gethash theme custom-theme-settings)))
+
 (defun mine-light-color-theme ()
   (interactive)
-  (color-theme-standard)
-  (set-face-background 'region "#ffec8b")
-  (set-face-background 'hl-line "#b4eeb4")
-  (set-face-foreground 'font-lock-doc-face "#d2691e")
-  (custom-set-faces    '(outline-2 ((t (:foreground "#006400" :bold t))))))
+  (apply-custom-theme-with-settings 'color-theme-standard))
 
 (defun mine-dark-color-theme ()
   (interactive)
-  (color-theme-merbivore)
-  (set-face-background 'hl-line "#3b3b3b")
-  (set-face-background 'region "#999966")
-  (set-face-foreground 'font-lock-comment-face "#d8e5c5")
-  (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t))))))
+  (apply-custom-theme-with-settings 'color-theme-merbivore))
 
-(mine-dark-color-theme)
+(apply-custom-theme-with-settings 'color-theme-merbivore)
 
 (provide 'mine-color-theme)
