847f70809b2ba1aab429046542b37ae1f75ba97f
Nick Parker <nickp@developernotes.com>
2010-10-28 20:09:01 -0500

    Adding toggle-line-numbers and toggle-highlight-line

--- mine/mine-defuns.el
+++ mine/mine-defuns.el
@@ -1,21 +1,35 @@
 
+(defun toggle-line-numbers ()
+  "Enable or disable line numbers globally"
+  (interactive)
+  (if global-linum-mode
+      (global-linum-mode -1)
+    (global-linum-mode 1)))
+
+(defun toggle-highlight-line ()
+  "Enable or disable highlighting of current line"
+  (interactive)
+  (if global-hl-line-mode
+      (global-hl-line-mode -1)
+    (global-hl-line-mode 1)))
+
 (defun beginning-of-line-or-back-to-indention ()
   (interactive)
   "This goes to back to indention or if already there beginning of line"
   (let ((previous-point (point)))
     (back-to-indentation)
     (if (equal previous-point (point))
-	(beginning-of-line))))
+        (beginning-of-line))))
 
 (defun mine-server-start ()
-	"Starts the emacs server, navigating to either a directory or file if set via environment variables"
-	(server-start)
-	(let ((dir  (getenv "EOPEN_DIR"))
+  "Starts the emacs server, navigating to either a directory or file if set via environment variables"
+  (server-start)
+  (let ((dir  (getenv "EOPEN_DIR"))
         (file (getenv "EOPEN_FILE")))
-  (if dir
-      (cd dir))
-  (if file
-      (find-file file))))
+    (if dir
+        (cd dir))
+    (if file
+        (find-file file))))
 
 (defun clean-up-buffer-or-region ()
   "Untabifies, indents and deletes trailing whitespace from buffer or region."
@@ -30,26 +44,26 @@
       (delete-trailing-whitespace))))
 
 (defun append-to-environment-variable (variable path)
-	(setenv variable (concat (format "%s:%s" (getenv variable) path)))) 
+  (setenv variable (concat (format "%s:%s" (getenv variable) path))))
 
 (defun kill-all-buffers ()
   "kill all buffers, leaving *scratch* only"
   (interactive)
   (mapcar (lambda (x) (kill-buffer x))
-					(buffer-list))
+          (buffer-list))
   (delete-other-windows))
 
 (defun kill-all-buffers-but-current ()
-	"kills all buffers except your current buffer"
-	(interactive)
-	(mapcar (lambda (x) (kill-buffer x))
-					(remove (current-buffer) (buffer-list)))
-	(delete-other-windows))
+  "kills all buffers except your current buffer"
+  (interactive)
+  (mapcar (lambda (x) (kill-buffer x))
+          (remove (current-buffer) (buffer-list)))
+  (delete-other-windows))
 
 (defun tail ()
-	"tail the file loaded within the current buffer"
-	(interactive)
-	(auto-revert-tail-mode))
+  "tail the file loaded within the current buffer"
+  (interactive)
+  (auto-revert-tail-mode))
 
 (defun string-replace (old-value new-value source)
   "replace old-value with new-value from source"
