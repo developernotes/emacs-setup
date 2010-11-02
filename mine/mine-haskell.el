a8e1751a4d1da6a257229ec79ee8e698c0b4142a
Nick Parker <nparker@geolearning.com>
2010-11-02 13:01:27 -0500

    Setting the indentation style for haskell

--- mine/mine-haskell.el
+++ mine/mine-haskell.el
@@ -3,9 +3,7 @@
 (load "haskell-site-file")
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
 
 (provide 'mine-haskell)
