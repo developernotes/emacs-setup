3e74fbcb83af28d64e41eabcc3ed70cc207c3593
Nick Parker <nparker@geolearning.com>
2010-11-04 22:50:44 -0500

    Adding a function to make it easier to execute a haskell script

--- mine/mine-haskell.el
+++ mine/mine-haskell.el
@@ -5,5 +5,15 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook '(lambda ()
+                                (define-key haskell-mode-map (kbd "C-c r") 'haskell-run-buffer-in-runghc)))
+
+(defun haskell-run-buffer-in-runghc()
+  (interactive)
+  (when (get-buffer "*runghc*")
+    (kill-buffer "*runghc*"))
+  (let ((args (list (buffer-file-name (current-buffer)))))
+    (apply 'make-comint "runghc" "runghc" nil args)
+    (display-buffer "*runghc*")))
 
 (provide 'mine-haskell)
