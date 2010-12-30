bc4cd73c5ea74ca028ca595e85a4b2ae26de50b9
Nick Parker <nickp@developernotes.com>
2010-12-30 15:37:57 -0600

    Adding additional keybindings for scala-mode

--- mine/mine-scala.el
+++ mine/mine-scala.el
@@ -6,5 +6,12 @@
 (require 'ensime)
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(add-hook 'scala-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "RET") 'newline-and-indent)
+             (c-subword-mode t)
+             (local-set-key (kbd "C-c C-b") 'scala-eval-buffer)
+             (local-set-key (kbd "C-c C-r") 'scala-eval-region)
+             (local-set-key (kbd "C-M-x") 'scala-eval-definition)))
 
 (provide 'mine-scala)
