3a71714b400be60a738dac8f85c9f4ec069a9a09
Nick Parker <nickp@developernotes.com>
2010-12-30 15:06:34 -0600

    Removing electric-return-if-match, was causing issues in ielm

--- mine/mine-lisp.el
+++ mine/mine-lisp.el
@@ -18,22 +18,6 @@
   "Configure default settings for a LISP environment"
   (paredit-mode +1)
   (turn-on-eldoc-mode)
-  (eldoc-add-command 'paredit-backward-delete 'paredit-close-round)
-  (local-set-key (kbd "RET") 'electrify-return-if-match)
-  (eldoc-add-command 'electrify-return-if-match))
-
-(defvar electrify-return-match "[\]}\)\"]"
-  "If this regexp matches the text after the cursor, do an \"electric\" return.")
-
-(defun electrify-return-if-match (arg)
-  "If the text after the cursor matches `electrify-return-match' then
-  open and indent an empty line between the cursor and the text.  Move the
-  cursor to the new line."
-  (interactive "P")
-  (let ((case-fold-search nil))
-    (if (looking-at electrify-return-match)
-        (save-excursion (newline-and-indent)))
-    (newline arg)
-    (indent-according-to-mode)))
+  (eldoc-add-command 'paredit-backward-delete 'paredit-close-round))
 
 (provide 'mine-lisp)
