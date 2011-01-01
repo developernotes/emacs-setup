b43b988d5b8abab0b5a7284a01296d54ffbf6b46
Nick Parker <nparker@geolearning.com>
2011-01-01 11:46:06 -0600

    Adjustments to get clojure-clr to work with mono

--- mine/mine-clojure-clr.el
+++ mine/mine-clojure-clr.el
@@ -1,13 +1,20 @@
 
-(defvar clojure-clr-repl-bin "Clojure.Main.exe")
+(defvar clojure-clr-repl-bin)
+(defvar clojure-clr-repl-args)
 (defconst clojure-clr-buffer-name "*Clojure-CLR*")
 
+(case system-type
+  ('windows-nt (setq clojure-clr-repl-bin "Clojure.Main.exe"
+                     clojure-clr-repl-args nil))
+  ('darwin     (setq clojure-clr-repl-bin "mono"
+                     clojure-clr-repl-args (list (expand-file-name "~/tools/clojure-clr/bin/Clojure.Main.exe")))))
+               
 (defun clojure-clr-run-interpreter ()
   "Launches a Clojure CLR interpreter"
   (interactive)
   (when (get-buffer clojure-clr-buffer-name)
     (kill-buffer clojure-clr-buffer-name))
-    (apply 'make-comint "Clojure-CLR" clojure-clr-repl-bin nil nil)
+    (apply 'make-comint "Clojure-CLR" clojure-clr-repl-bin nil clojure-clr-repl-args)
     (display-buffer clojure-clr-buffer-name))
 
 (defun clojure-clr-eval-buffer ()
