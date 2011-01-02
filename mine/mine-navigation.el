f92064fe8769b06f3f67d85c3e942ce7cbc3249c
Nick Parker <nickp@developernotes.com>
2011-01-02 15:11:20 -0600

    Adding mark commands

--- mine/mine-navigation.el
+++ mine/mine-navigation.el
@@ -139,6 +139,22 @@
            (set-window-start w2 s1))))
   (other-window 1))
 
+;; mark commands
+(defun push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+(global-set-key (kbd "C-`") 'push-mark-no-activate)
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "M-`") 'jump-to-mark)
+
 (defun mine-goto-symbol-at-point ()
   "Will navigate to the symbol at the current point of the cursor"
   (interactive)
