b482e10bf213c2f5b9da904713db89d3ed575987
Nick Parker <nparker@geolearning.com>
2011-01-04 10:20:56 -0600

    Auto reverting dired buffer by default

--- mine/mine-dired.el
+++ mine/mine-dired.el
@@ -6,6 +6,8 @@
             (define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
             (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))))
 
+(setq dired-auto-revert-buffer t)
+
 (defun dired-open-marked-files ()
   "Opens all marked files"
   (interactive)
