87e3d2f5b7168456ac80e1b73ffc82fb42df0ccc
Nick Parker <nickp@developernotes.com>
2011-03-12 13:27:29 -0600

    Adding function to toggle the vc interface

--- mine/mine-vc.el
+++ mine/mine-vc.el
@@ -2,4 +2,17 @@
 (add-path "site-lisp/magit/")
 (autoload 'magit-status "magit" "use magit git magic" t)
 
+(defun mine-toggle-vc ()
+  "Enable or disable emacs version control interface"
+  (interactive)
+  (if (eq vc-handled-backends nil)
+      (progn
+        (setq vc-handled-backends '(CVS SVN Bzr Git Hg))
+        (message "vc interface enabled"))
+    (progn
+      (setq vc-handled-backends nil)
+      (message "vc interface disabled"))))
+
+(mine-toggle-vc)
+
 (provide 'mine-vc)
