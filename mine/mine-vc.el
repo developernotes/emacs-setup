60ed0dc80fd8a5d2fd511b949babfc9c86b98380
Nick Parker <nickp@developernotes.com>
2011-03-12 14:47:06 -0600

    Cleaning up mine-toggle-vc

--- mine/mine-vc.el
+++ mine/mine-vc.el
@@ -3,15 +3,11 @@
 (autoload 'magit-status "magit" "use magit git magic" t)
 
 (defun mine-toggle-vc ()
-  "Enable or disable emacs version control interface"
   (interactive)
-  (if (eq vc-handled-backends nil)
-      (progn
-        (setq vc-handled-backends '(CVS SVN Bzr Git Hg))
-        (message "vc interface enabled"))
-    (progn
-      (setq vc-handled-backends nil)
-      (message "vc interface disabled"))))
+  "Enable or disable emacs version control interface"
+  (let ((enabled (not (eq vc-handled-backends nil))))
+    (setf vc-handled-backends (if enabled nil '(CVS SVN Bzr Git Hg)))
+    (message "vc interface %s" (if enabled "disabled" "enabled"))))
 
 (mine-toggle-vc)
 
