5f6e97b5d6e5ac4b34642593c22bd58e71f29271
Nick Parker <nickp@developernotes.com>
2011-06-04 13:50:11 -0500

    Advice to automatically close completions for comint-mode

--- mine/mine-advice.el
+++ mine/mine-advice.el
@@ -20,4 +20,26 @@
     ad-do-it
     (and line (goto-line line))))
 
+(defun comint-close-completions ()
+  (if comint-dynamic-list-completions-config
+      (progn
+        (set-window-configuration comint-dynamic-list-completions-config)
+        (setq comint-dynamic-list-completions-config nil))))
+
+(defadvice comint-send-input (after close-completions activate)
+  (comint-close-completions))
+
+(defadvice comint-dynamic-complete-as-filename (after close-completions activate)
+  (if ad-return-value (comint-close-completions)))
+
+(defadvice comint-dynamic-simple-complete (after close-completions activate)
+  (if (member ad-return-value '('sole 'shortest 'partial))
+      (comint-close-completions)))
+
+(defadvice comint-dynamic-list-completions (after close-completions activate)
+    (comint-close-completions)
+    (if (not unread-command-events)
+        ;; comint's "Type space to flush" swallows space. put it back in.
+        (setq unread-command-events (listify-key-sequence " "))))
+
 (provide 'mine-advice)
