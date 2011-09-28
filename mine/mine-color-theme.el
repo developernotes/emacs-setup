389598768c667c8d7c11276c02b6ea219ec396e9
Nick Parker <nparker@zetetic.net>
2011-09-27 21:12:11 -0500

    Porting my light color theme to emacs 24 and loading when correct emacs version available

--- mine/mine-color-theme.el
+++ mine/mine-color-theme.el
@@ -1,53 +1,60 @@
 
-(add-path "site-lisp/themes")
-(add-path "site-lisp/color-theme-6.6.0")
+(when (string-match "^24\." emacs-version)
+  (progn
+    (add-to-list 'custom-theme-load-path (concat emacs-root "site-lisp/themes"))
+    (load-file (concat emacs-root "site-lisp/themes/standard-theme.el"))))
 
-(require 'color-theme)
-(require 'color-theme-zen-and-art)
+(when (string-match "^23\." emacs-version)
+  (progn
+    (add-path "site-lisp/themes")
+    (add-path "site-lisp/color-theme-6.6.0")
 
-(color-theme-initialize)
+    (require 'color-theme)
+    (require 'color-theme-zen-and-art)
 
-(setq custom-theme-settings
-      (progn
-        (let ((theme-settings (make-hash-table :test 'equal)))
-          (puthash 'color-theme-standard
-                   '((set-face-background 'region "#ffec8b")
-                     (set-face-foreground 'font-lock-doc-face "#d2691e")
-                     (custom-set-faces    '(outline-2 ((t (:foreground "#006400" :bold t))))))
-                   theme-settings)
-          (puthash 'color-theme-tango-2
-                   '((set-face-background 'region "#999966"))
-                   theme-settings)
-          (puthash 'color-theme-merbivore
-                   '((set-face-background 'region "#999966")
-                     (set-face-foreground 'font-lock-comment-face "#d8e5c5")
-                     (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t)))))
-                     (custom-set-faces    '(modeline ((t (:background "#3F3B3B" :foreground "white")))))
-                     (custom-set-faces    '(modeline-buffer-id ((t (:background "#3F3B3B" :foreground "white")))))
-                     (custom-set-faces    '(modeline-mousable ((t (:background "#a5baf1" :foreground "black")))))
-                     (custom-set-faces    '(modeline-mousable-minor-mode ((t (:background "#a5baf1" :foreground "black")))))
-                     (custom-set-faces    '(isearch ((t (:background "#555555")))))
-                     (custom-set-faces    '(minibuffer-prompt ((t (:bold t :foreground "#ff6600"))))))
-                   theme-settings)
-          (puthash 'color-theme-zen-and-art
-                   '((set-face-background 'region "#999966")
-                     (set-face-foreground 'font-lock-comment-face "#d8e5c5")
-                     (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t))))))
-                   theme-settings)
-          theme-settings)))
+    (color-theme-initialize)
 
-(defun apply-custom-theme-with-settings (theme)
-  (funcall theme)
-  (mapcar 'eval (gethash theme custom-theme-settings)))
+    (setq custom-theme-settings
+          (progn
+            (let ((theme-settings (make-hash-table :test 'equal)))
+              (puthash 'color-theme-standard
+                       '((set-face-background 'region "#ffec8b")
+                         (set-face-foreground 'font-lock-doc-face "#d2691e")
+                         (custom-set-faces    '(outline-2 ((t (:foreground "#006400" :bold t))))))
+                       theme-settings)
+              (puthash 'color-theme-tango-2
+                       '((set-face-background 'region "#999966"))
+                       theme-settings)
+              (puthash 'color-theme-merbivore
+                       '((set-face-background 'region "#999966")
+                         (set-face-foreground 'font-lock-comment-face "#d8e5c5")
+                         (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t)))))
+                         (custom-set-faces    '(modeline ((t (:background "#3F3B3B" :foreground "white")))))
+                         (custom-set-faces    '(modeline-buffer-id ((t (:background "#3F3B3B" :foreground "white")))))
+                         (custom-set-faces    '(modeline-mousable ((t (:background "#a5baf1" :foreground "black")))))
+                         (custom-set-faces    '(modeline-mousable-minor-mode ((t (:background "#a5baf1" :foreground "black")))))
+                         (custom-set-faces    '(isearch ((t (:background "#555555")))))
+                         (custom-set-faces    '(minibuffer-prompt ((t (:bold t :foreground "#ff6600"))))))
+                       theme-settings)
+              (puthash 'color-theme-zen-and-art
+                       '((set-face-background 'region "#999966")
+                         (set-face-foreground 'font-lock-comment-face "#d8e5c5")
+                         (custom-set-faces    '(outline-2 ((t (:foreground "#a5f26e" :bold t))))))
+                       theme-settings)
+              theme-settings)))
 
-(defun mine-light-color-theme ()
-  (interactive)
-  (apply-custom-theme-with-settings 'color-theme-standard))
+    (defun apply-custom-theme-with-settings (theme)
+      (funcall theme)
+      (mapcar 'eval (gethash theme custom-theme-settings)))
 
-(defun mine-dark-color-theme ()
-  (interactive)
-  (apply-custom-theme-with-settings 'color-theme-zen-and-art))
+    (defun mine-light-color-theme ()
+      (interactive)
+      (apply-custom-theme-with-settings 'color-theme-standard))
 
-(mine-light-color-theme)
+    (defun mine-dark-color-theme ()
+      (interactive)
+      (apply-custom-theme-with-settings 'color-theme-zen-and-art))
+
+    (mine-light-color-theme)))
 
 (provide 'mine-color-theme)
---
+++ site-lisp/themes/standard-theme.el
@@ -0,0 +1,54 @@
+;;; standard-theme.el --- Custom face theme for Emacs
+
+;; Copyright (C) 2010 Nick Parker.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(deftheme standard
+  "Standard light color theme")
+
+(custom-theme-set-faces
+ 'standard
+ '(default ((t (:background "#ffffff" :foreground "#000000"))))
+ '(cursor ((t (:background "#000000" :foreground "#ffffff"))))
+ '(region ((t (:background "#eedc82" :weight bold))))
+ '(mode-line ((t (:background "#000000" :foreground "#ffffff"))))
+ '(mode-line-inactive ((t (:background "#000000" :foreground "#ffffff"))))
+ '(fringe ((t (:background "#f2f2f2"))))
+ '(minibuffer-prompt ((t (:foreground "#0000cd" :weight bold))))
+ '(font-lock-builtin-face ((t (:foreground "#ba55d3" :weight bold))))
+ '(font-lock-comment-face ((t (:foreground "#b22222" :weight bold))))
+ '(font-lock-constant-face ((t (:foreground "#008b8b" :weight bold))))
+ '(font-lock-function-name-face ((t (:foreground "#0000ff" :weight bold))))
+ '(font-lock-keyword-face ((t (:foreground "#a020f0" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "#8b2252" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "#228b22" :weight bold))))
+ '(font-lock-variable-name-face ((t (:foreground "#a0522d" :weight bold))))
+ '(font-lock-warning-face ((t (:foreground "#ff0000" :weight bold))))
+ '(isearch ((t (:background "#cd00cd" :foreground "#b0e2ff" :weight bold))))
+ '(lazy-highlight ((t (:background "#afeeee" :weight bold))))
+ '(link ((t (:foreground "#0000ff" :underline t :weight bold))))
+ '(link-visited ((t (:foreground "#8b008b" :underline t :weight bold))))
+ '(button ((t (:underline t :weight bold))))
+ '(header-line ((t (:background "#e5e5e5" :foreground "#333333")))))
+
+(provide-theme 'standard)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; standard-theme.el  ends here
