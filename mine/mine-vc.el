37de9aff1e57abb0cf677f4f89a2dd43d568f360
Nick Parker <nparker@zetetic.net>
2011-10-02 12:53:09 -0500

    Rewording message

--- mine/mine-vc.el
+++ mine/mine-vc.el
@@ -11,7 +11,7 @@
   "Enable or disable emacs version control interface"
   (let ((enabled (not (eq vc-handled-backends nil))))
     (setf vc-handled-backends (if enabled nil '(CVS SVN Bzr Git Hg)))
-    (message "vc interface %s" (if enabled "disabled" "enabled"))))
+    (message "version control interface %s" (if enabled "disabled" "enabled"))))
 
 (mine-toggle-vc)
 
