e60b92ed756f44579ea76368183fdf84c1d3177c
Nick Parker <nparker@zetetic.net>
2011-10-02 13:43:21 -0500

    Removing various elpa packages

--- elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19193 10113
-;;;;;;  474498))
-
-;;;***
-
-(provide 'findr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
