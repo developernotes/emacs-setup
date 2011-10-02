7e3c4173fa82d9aeaaddf892ab91ea87e155215f
Nick Parker <nparker@zetetic.net>
2011-10-02 12:22:55 -0500

    Autoloading textile mode

--- mine/mine-html.el
+++ mine/mine-html.el
@@ -1,5 +1,5 @@
-(require 'textile-mode)
 
+(autoload 'textile-mode "textile-mode" "Mode for editing textile documents" nil)
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.textile$" . textile-mode))
 (add-to-list 'auto-mode-alist '("\\.xhtml$" . html-mode))
