90c7028d0155404e185690e8baac9dfaf2016295
Nick Parker <nparker@zetetic.net>
2011-10-02 12:52:51 -0500

    Autoloading showoff mode

--- mine/mine-showoff.el
+++ mine/mine-showoff.el
@@ -1,7 +1,8 @@
 
 (add-path "site-lisp/showoff-mode")
 
-(require 'showoff-mode)
+;;(require 'showoff-mode)
+(autoload 'showoff-mode "showoff-mode" "Mode for editing showoff documents" t)
 (add-to-list 'auto-mode-alist '("\\.md$" . showoff-mode))
 
 (provide 'mine-showoff)
