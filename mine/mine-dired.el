64fd3053e3690e9f8835e509ec2df57356bb1c65
Nick Parker <nparker@zetetic.net>
2011-10-21 16:07:05 -0500

    Allow going to the top and bottom of files in dired a little easier

--- mine/mine-dired.el
+++ mine/mine-dired.el
@@ -3,6 +3,8 @@
 
 (add-hook 'dired-mode-hook
           (lambda ()
+            (define-key dired-mode-map (kbd "M->") 'end-of-dired-buffer)
+            (define-key dired-mode-map (kbd "M-<") 'beginning-of-dired-buffer)
             (define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
             (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))))
 
@@ -13,5 +15,19 @@
   (interactive)
   (dolist (file (dired-get-marked-files))
     (find-file file)))
+
+(defun beginning-of-dired-buffer ()
+  "Move point to first line containing a filename"
+  (interactive)
+  (goto-char (point-min))
+  (next-line 4)
+  (dired-move-to-filename))
+
+(defun end-of-dired-buffer ()
+  "Move point to last line of buffer, place cursor on filename"
+  (interactive)
+  (goto-char (point-max))
+  (previous-line)
+  (dired-move-to-filename))
 
 (provide 'mine-dired)
