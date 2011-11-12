fe94876d40bc96cfcc41dbfee163737ab6d91bde
Nick Parker <nparker@zetetic.net>
2011-11-12 10:35:13 -0600

    Removing additional geiser references

--- mine/mine-lisp.el
+++ mine/mine-lisp.el
@@ -2,9 +2,6 @@
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
 
-(setq scheme-program-name "guile")
-(load (concat emacs-root "site-lisp/geiser/elisp/geiser-load"))
-
 (add-hook 'emacs-lisp-mode-hook
           'mine-default-lisp-settings)
 
