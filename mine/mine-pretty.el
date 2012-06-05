128547a5f1d2def5db0a60b352f36349663b7e5e
Nick Parker <nparker@zetetic.net>
2012-06-05 08:06:50 -0500

    Bumping the font size

--- mine/mine-pretty.el
+++ mine/mine-pretty.el
@@ -9,7 +9,7 @@
 										 mine-font-name-large "DejaVu Sans Mono-18:bold"))
 	('gnu/linux  (setq mine-font-name-normal "DejaVu Sans Mono-12:bold"
 										 mine-font-name-large "DejaVu Sans Mono-18:bold"))
-  ('darwin     (setq mine-font-name-normal "Ubuntu Mono-16"
+  ('darwin     (setq mine-font-name-normal "Ubuntu Mono-18"
 										 mine-font-name-large "DejaVu Sans Mono-18:bold"))
   ('cygwin     (setq mine-font-name-normal ""
                      mine-font-name-large "")))
