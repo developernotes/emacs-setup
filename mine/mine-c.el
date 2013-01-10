135a95e78cc3d9e3069ec4151bef52cf664193a8
Nick Parker <nparker@zetetic.net>
2013-01-10 17:53:29 -0600

    adjustments for C

--- mine/mine-c.el
+++ mine/mine-c.el
@@ -1,6 +1,11 @@
 
 (add-hook 'c-mode-common-hook
-  (lambda ()
-    (local-set-key (kbd "C-c f") 'ff-find-other-file)))
+          (lambda ()
+            (local-set-key (kbd "C-c f") 'ff-find-other-file)))
+
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (set (make-local-variable 'cc-other-file-alist)
+                 '(("\\.m\\'" (".h")) ("\\.h\\'" (".m" ".c" ".cpp"))))))
 
 (provide 'mine-c)
