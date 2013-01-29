255af10550a3f7b4526218d7704bdc5a6ab03a10
Nick Parker <nparker@zetetic.net>
2013-01-29 09:15:21 -0600

    Adjustments to magit

--- init/init-magit.el
+++ init/init-magit.el
@@ -1,5 +1,5 @@
 
-(global-set-key "\C-c\m" 'magit-status)
+(require 'magit)
 
 (when (eq window-system nil)
   (make-empty-face 'magit-item-highlight)
@@ -12,4 +12,17 @@
     (setf vc-handled-backends (if enabled nil '(CVS SVN Bzr Git Hg)))
     (message "Version control interface %s" (if enabled "disabled" "enabled"))))
 
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
 (mine-toggle-vc)
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(global-set-key "\C-c\m" 'magit-status)
