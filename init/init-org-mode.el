f83d21a55fc415e2ca3c93789f883dcb337f7dbb
Nick Parker <nparker@zetetic.net>
2013-04-08 09:03:44 -0500

    Changes to org-mode

--- init/init-org-mode.el
+++ init/init-org-mode.el
@@ -130,32 +130,12 @@
           (org-agenda-files '("~/org/gtd-items.org" "~/org/work-notes.org"))
           (org-agenda-sorting-strategy '(priority-down time-up tag-up))))))
 
-(custom-set-faces
- '(outline-1 ((t (:foreground "#D6B163" :bold t))))
- '(outline-2 ((t (:foreground "#A5F26E" :bold t))))
- '(outline-3 ((t (:foreground "#B150E7" :bold nil))))
- '(outline-4 ((t (:foreground "#529DB0" :bold nil))))
- '(outline-5 ((t (:foreground "#CC7832" :bold nil))))
- '(org-level-1 ((t (:inherit outline-1))))
- '(org-level-2 ((t (:inherit outline-2))))
- '(org-level-3 ((t (:inherit outline-3))))
- '(org-level-4 ((t (:inherit outline-4))))
- '(org-level-5 ((t (:inherit outline-5))))
- '(org-agenda-date ((t (:inherit font-lock-type-face))))
- '(org-agenda-date-weekend ((t (:inherit org-agenda-date))))
- '(org-scheduled-today ((t (:foreground "#ff6ab9" :italic t))))
- '(org-scheduled-previously ((t (:foreground "#d74b4b"))))
- '(org-upcoming-deadline ((t (:foreground "#d6ff9c"))))
- '(org-warning ((t (:foreground "#8f6aff" :italic t))))
- '(org-date ((t (:inherit font-lock-constant-face))))
- '(org-tag ((t (:inherit font-lock-comment-delimiter-face))))
- '(org-hide ((t (:foreground "#191919"))))
- '(org-done ((t (:background "DarkGreen" :foreground "white"
-                             :box (:line-width 1 :style released-button)))))
- '(org-column ((t (:background "#222222"))))
- '(org-column-title ((t (:background "DarkGreen"
-                                     :foreground "white" :bold t
-                                     :box (:line-width 1 :style released-button))))))
+ (custom-set-faces
+  '(outline-2 ((t (:foreground "#cc7832"))))
+  '(org-level-2 ((t (:inherit outline-2))))
+  '(org-scheduled-today ((t (:foreground "#ff6ab9" :italic t))))
+  '(org-done ((t (:background "DarkGreen" :foreground "white"
+                              :box (:line-width 1 :style released-button))))))
 
 (setq org-todo-keyword-faces
       '(("IN-PROGRESS" . (:foreground "white" :background "#E9AB17"))
