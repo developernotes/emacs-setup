e02b9f186ae159740903d46aec657f77a2b2eca1
Nick Parker <nparker@zetetic.net>
2017-08-30 16:56:03 -0500

    Disable fill column indicator for certain modes

--- init/custom/init-fill-column-indicator.el
+++ init/custom/init-fill-column-indicator.el
@@ -1,3 +1,9 @@
 (setq fci-rule-column 80)
-(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(define-globalized-minor-mode global-fci-mode fci-mode
+  (lambda ()
+    (when (not (or
+                (eq major-mode 'eshell-mode)
+                (eq major-mode 'minibuffer-inactive-mode)))
+      (fci-mode 1))))
+
 (global-fci-mode 1)
